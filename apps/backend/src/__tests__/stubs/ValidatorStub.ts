2e35482cb5981ea0092828cb0d9858d930a1a97c
Cássio <cassiojr0108@gmail.com>
2026-06-03 13:45:25 -0300

    test: extend validator stub for course tests

--- apps/backend/src/__tests__/stubs/ValidatorStub.ts
+++ apps/backend/src/__tests__/stubs/ValidatorStub.ts
@@ -1,6 +1,15 @@
 import type { Validator } from '@/services/contracts/Validator';
 
 export class ValidatorStub implements Validator {
+  isValidTitle(_: string): boolean {
+    return true;
+  }
+  isValidDescription(_: string): boolean {
+    return true;
+  }
+  isValidShortDescription(_: string): boolean {
+    return true;
+  }
   isEmail(_: string): boolean {
     return true;
   }
