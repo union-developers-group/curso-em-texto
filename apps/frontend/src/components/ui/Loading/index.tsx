23b86e29d79e689fc80f97ee4a0f06c3c1a8f90d
Gabriel Porto <gpalmenara@gmail.com>
2026-06-12 13:16:59 -0300

    chore(loading): border-3 para border-[3px]

--- apps/frontend/src/components/ui/Loading/index.tsx
+++ apps/frontend/src/components/ui/Loading/index.tsx
@@ -8,7 +8,7 @@ export const loadingVariants = cva(
     variants: {
       size: {
         sm: 'h-6 w-6 border-2',
-        md: 'h-10 w-10 border-3',
+        md: 'h-10 w-10 border-[3px]',
         lg: 'h-14 w-14 border-4',
       },
     },
